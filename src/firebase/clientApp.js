f5bd981caf536f5a7234a6ac2256c580325434d7
Karolis Ra <73240914+karolis-ra@users.noreply.github.com>
2025-03-25 19:02:13 +0200

    Update clientApp.js

--- src/firebase/clientApp.js
+++ src/firebase/clientApp.js
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBPz5FG0SYPWo-40IhOi4zR-C4_Mx_YssY",
-    authDomain: "sportbuddy-68397.firebaseapp.com",
-    projectId: "sportbuddy-68397",
-    storageBucket: "sportbuddy-68397.appspot.com",
-    messagingSenderId: "659631696507",
-    appId: "1:659631696507:web:b54b0872f7de3b3ada50de",
-};
+
 
 initializeApp(firebaseConfig)
 
